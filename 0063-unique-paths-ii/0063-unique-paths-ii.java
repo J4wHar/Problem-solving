df6c452d7196af386a2fa74c78de480d7c34125b
J4wHar <98396181+J4wHar@users.noreply.github.com>
2023-08-12 04:51:04 +0100

    Time: 1 ms (17.01%), Space: 43.2 MB (6.86%) - LeetHub

---
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -0,0 +1,32 @@
+class Solution {
+    private int n, m;
+    private int[][] memo = new int[100][100];
+    private boolean safe(int x, int y){
+        return x>=0 && x<n && y>=0 && y<m;
+    }
+    private int solve(int i, int j, int[][] grid){
+        if(i==n-1 && j==m-1){
+            if(grid[i][j]==1){
+                return 0;
+            }
+            return 1;
+        }
+        if(!safe(i, j) || grid[i][j]==1){
+            return 0;
+        }
+        if(memo[i][j] != -1){
+            return memo[i][j];
+        }
+        return memo[i][j] = solve(i+1, j, grid) + solve(i, j+1, grid);
+    }
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        n = obstacleGrid.length;
+        m = obstacleGrid[0].length;
+        for(int i=0;i<n;++i){
+            for(int j=0;j<m;++j){
+                memo[i][j] = -1;
+            }
+        }
+        return solve(0, 0, obstacleGrid);
+    }
+}
