9fb24b3bb37c4a782c115c94e0d8f4496cc5a2a5
J4wHar <98396181+J4wHar@users.noreply.github.com>
2023-08-13 19:33:43 +0100

    Time: 320 ms (5.23%), Space: 147 MB (5.23%) - LeetHub

---
+++ 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    map<int,bool> memo;
+    int n;
+    bool solve(int i, vector<int>& nums){
+        if(i == n){
+            return true;
+        }
+        if(memo.count(i)){
+            return memo[i];
+        }
+        bool ans = false;
+        if(i<n-1 && nums[i]==nums[i+1]){
+            ans |= solve(i+2, nums);
+        }
+        if(i<n-2 && ((nums[i]==nums[i+1] && nums[i+1]==nums[i+2]) || (nums[i]+1==nums[i+1] && nums[i+1]==nums[i+2]-1))){
+            ans |= solve(i+3, nums);
+        }
+        return memo[i] = ans;
+    }
+    bool validPartition(vector<int>& nums) {
+        n = nums.size();
+        return solve(0, nums);
+    }
+};
