abc4b77f41e192aa37f8b2ac242be024f119de5c
J4wHar <98396181+J4wHar@users.noreply.github.com>
2023-08-27 23:53:05 +0100

    Time: 320 ms (21.86%), Space: 31.4 MB (22.90%) - LeetHub

--- 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.cpp
+++ 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.cpp
@@ -16,9 +16,6 @@ public:
         return memo[i][cur] = solve(i+1, cur, pairs);
     }
     static bool compare(vector<int>& v1, vector<int>& v2){
-        if(v1[1] == v2[1]){
-            return v1[0] < v2[0];
-        }
         return v1[1] < v2[1];
     }
     int findLongestChain(vector<vector<int>>& pairs) {
