a639aa5abb298fbf29e9e575c85e96a029353954
J4wHar <98396181+J4wHar@users.noreply.github.com>
2023-09-23 01:40:19 +0100

    Time: 123 ms (45.94%), Space: 23.1 MB (39.99%) - LeetHub

---
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    bool isPref(string s, string t){
+        int n = s.size();
+        int m = t.size();
+        int i=0, j=0;
+        while(i<n && j<m){
+            if(s[i] == t[j]){
+                ++j;
+            }
+            ++i;
+        }
+        return j == m;
+    }
+    int longestStrChain(vector<string>& words) {
+        int n = words.size();
+        int dp[n];
+        int ans = 0;
+        for(int i=0;i<n;++i){
+            dp[i] = 1;
+        }
+        sort(words.begin(), words.end(), [](string s1, string s2){
+            return s1.size() < s2.size();
+        });
+        for(int i=n-1;i>=0;--i){
+            for(int j=i-1;j>=0;--j){
+                if(j != i){
+                    if(words[i].size() == words[j].size()+1 && isPref(words[i], words[j])){
+                        dp[j] = max(dp[j], 1+dp[i]);
+                    }
+                }
+            }
+        }
+        for(int i=0;i<n;++i){
+            ans = max(ans, dp[i]);
+        }
+        return ans;
+    }
+};
