6c1885ef575979ee0df9d95f29646dced8b6f017
J4wHar <98396181+J4wHar@users.noreply.github.com>
2023-09-23 18:42:04 +0100

    Time: 1141 ms (16.51%), Space: 192 MB (9.24%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,27 +1,23 @@
 class Solution {
 public:
-    bool bs(int l, int r, int target, vector<int>& arr){
-        int m = l+(r-l)/2;
-        if(r<l){
-            return false;
-        }
-        if(arr[m] == target){
-            return true;
-        }
-        if(arr[m] < target){
-            return bs(m+1, r, target, arr);
-        }
-        return bs(l, m-1, target, arr);
-    }
     vector<vector<int>> threeSum(vector<int>& nums) {
         set<vector<int>> res;
         vector<vector<int>> ans;
         sort(nums.begin(), nums.end());
         int n = nums.size();
-        for(int i=0;i<n;++i){
-            for(int j=i+1;j<n-1;++j){
-                if(bs(j+1, n-1, -nums[i]-nums[j], nums)){
-                    res.insert({nums[i], nums[j], -nums[i]-nums[j]});
+        for(int i=0;i<n-2;++i){
+            int l = i+1;
+            int r = n-1;
+            while(l < r){
+                int x = nums[i] + nums[l] + nums[r];
+                if(x > 0){
+                    --r;
+                }else if(x < 0){
+                    ++l;
+                }else{
+                    res.insert({nums[i], nums[l], nums[r]});
+                    ++l;
+                    --r;
                 }
             }
         }
