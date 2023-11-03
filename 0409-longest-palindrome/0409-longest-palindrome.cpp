8fa925d23c6df79e5895dff0a837099e0acffeaa
J4wHar <98396181+J4wHar@users.noreply.github.com>
2023-11-03 23:48:51 +0100

    Time: 5 ms (31.90%), Space: 7.3 MB (7.10%) - LeetHub

---
+++ 0409-longest-palindrome/0409-longest-palindrome.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    static bool compare(pair<char, int> p1, pair<char, int> p2){
+        return p1.second > p2.second;
+    }
+    int longestPalindrome(string s) {
+        map<char, int> occ;
+        vector<pair<char, int>> v;
+        bool impAvailable = true;
+        int ans = 0;
+        for(auto &x: s){
+            ++occ[x];
+        }
+        for(auto &x: occ){
+            v.push_back(x);
+        }
+        sort(v.begin(), v.end(), compare);
+        for(auto &x: v){
+            if(x.second & 1){
+                if(impAvailable){
+                    ans += x.second;
+                    impAvailable = false;
+                }else{
+                    ans += x.second - 1;
+                }
+            }else{
+                ans += x.second;
+            }
+        }
+        return ans;
+    }
+};
