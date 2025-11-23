8d1dcf4667e640add88018cfb6009c3efc05ef35
J4wHar <98396181+J4wHar@users.noreply.github.com>
2025-11-23 23:57:57 +0100

    Time: 359 ms (5.02%), Space: 29.3 MB (77.18%) - LeetHub

---
+++ 1262-greatest-sum-divisible-by-three/1262-greatest-sum-divisible-by-three.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    static const int N = 4e4 + 3;
+    int dp[N][3];
+    int getValue(int i, int j, vector<int>& nums) {
+        return dp[i][j] == 0 ? 0 : dp[i][j] + nums[i];
+    }
+    int maxSumDivThree(vector<int>& nums) {
+        int n = nums.size();
+        memset(dp, 0, sizeof dp);
+        for (int i = 1; i <= n; ++i) {
+            if (nums[i - 1] % 3 == 0) {
+                dp[i][0] = dp[i - 1][0] + nums[i - 1];
+                for (int j = 1; j < 3; ++j) {
+                    dp[i][j] = getValue(i - 1, j, nums);
+                }
+            } else if (nums[i - 1] % 3 == 1) {
+                dp[i][0] = max(dp[i - 1][0], getValue(i - 1, 2, nums));
+                dp[i][1] = max(dp[i - 1][1], dp[i - 1][0] + nums[i - 1]);
+                dp[i][2] = max(dp[i - 1][2], getValue(i - 1, 1, nums));
+            } else {
+                dp[i][0] = max(dp[i - 1][0], getValue(i - 1, 1, nums));
+                dp[i][1] = max(dp[i - 1][1], getValue(i - 1, 2, nums));
+                dp[i][2] = max(dp[i - 1][2], dp[i - 1][0] + nums[i - 1]);
+            }
+        }
+
+        for (int i = 1; i <= n; ++i) {
+            cout << " " << dp[i][0];
+        }
+
+        return dp[n][0];
+    }
+};
