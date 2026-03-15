bd3370d7532b28b13e70b0e1fc2cbef6e449a36c
J4wHar <98396181+J4wHar@users.noreply.github.com>
2026-03-15 21:57:47 +0100

    Time: 0 ms (100%), Space: 14.1 MB (78.87%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
@@ -1,27 +1,17 @@
 class Solution {
 public:
-    int findMin(vector<int>& nums, int l = 0, int r = -1) {
-        cout << l << " " << r << endl;
-        if (r == -1) {
-            r = nums.size() - 1;
-        }
-        if (l == r) {
-            return nums[l];
-        }
-        if (r == l + 1) {
-            return min(nums[l], nums[r]);
-        }
-        if (r == l + 2) {
-            return min(min(nums[l], nums[l + 1]), nums[r]);
-        }
-        int m = l + ((r - l) / 2);
-        if (nums[l] < nums[m] && nums[m] < nums[r]) {
-            return findMin(nums, l, m - 1);
-        }
-        if (nums[m] > nums[r]) {
-            return findMin(nums, m, r);
+    int findMin(vector<int>& nums) {
+        int l = 0;
+        int r = nums.size() - 1;
+        while (l < r) {
+            int m = l + (r - l) / 2;
+            if (nums[m] > nums[r]) {
+                l = m + 1;
+            } else {
+                r = m;
+            }
         }
 
-        return findMin(nums, l, m);
+        return nums[l];
     }
 };
